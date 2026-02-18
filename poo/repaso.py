69653eac79c4fd11dd876842490ca05a9b0c6762
padillasam <panarick@gmail.com>
2026-02-18 16:26:45 -0500

    fundamentos de programación

---
+++ poo/repaso.py
@@ -0,0 +1,21 @@
+#indentación
+# def suma(x,y):
+#     return x+y
+# print(suma(3,4))
+# print(suma())
+x=9
+y=90
+if x>y:
+    print(x)
+elif y>x:
+    print(y)
+else:
+    print("son iguales")
+
+#match-case
+for i in range(0,10,2):
+    print(i)
+
+while z<10:
+    print(z)
+else
